51d870732cdb16398fa9f23f77b9e0386dfbd64c
Fabio <fabio.dadda@unimib.it>
2025-02-13 18:32:57 +0100

    change colors and logo

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -10,13 +10,13 @@ export const Header = (props: HeaderProps) => {
   return (
     <header
       className={cn(
-        "flex h-16 shrink-0 items-center gap-4 border-b bg-forest-green-700 px-4",
+        "flex h-16 shrink-0 items-center gap-2 border-b bg-forest-green-700 px-4",
         className,
       )}
     >
       <MicareLogo />
       <h1 className="text-base font-bold text-white">
-        {mainContent.validation ? "LLMind Chat Validator" : "LLMind"}
+        {mainContent.validation ? "Chat Validator" : "LLMind"}
       </h1>
     </header>
   );
